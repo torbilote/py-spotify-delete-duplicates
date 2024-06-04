c300c0f99ce6eaa66c225efb79e2c5b6337ac9be
nlipinski <norbert.lipinski@forecast.global>
2024-06-04 12:08:30 +0100

    ordered tracks by added at

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 auth_url = 'https://accounts.spotify.com/api/token'
@@ -12,4 +13,8 @@ base_url = 'https://api.spotify.com/v1'
 client_id = os.getenv('SPOTIFY_CLIENT_ID')
 client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
 
-playlist_id = '0834B53OKVRls7gMuDwmak'
+playlists = {
+    'main_stage' : '0834B53OKVRls7gMuDwmak',
+    'old stage' : '116x1msBQ3D1snxMJi7hUP',
+    'green stage' : '4NECTGO4FlA8cElx9v0UlN',
+}
--- app/main.py
+++ app/main.py
@@ -1,62 +1,15 @@
-import requests
 from app import config as cfg
-import json
+from app import utils as utils
 
-initial_request = requests.post(
-    url=cfg.auth_url,
-    headers=cfg.auth_headers,
-    data=f"grant_type=client_credentials&client_id={cfg.client_id}&client_secret={cfg.client_secret}"
-)
+if __name__ == "__main__":
+    access_token = utils.get_access_token()
 
-bearer_access_token = initial_request.json().get('access_token')
+    duplicates = dict()
 
-#1 get_specific_playlist
-get_playlist = requests.get(
-    url=f"{cfg.base_url}/playlists/{cfg.playlist_id}",
-    headers={ "Authorization": f"Bearer {bearer_access_token}" },
-    params={ "fields": "tracks.items(track(id))" }
-)
+    for playlist_name, playlist_id in cfg.playlists.items():
+        tracks = utils.get_playlist_tracks(playlist_id, access_token)
+        duplicated_tracks_list = utils.find_duplicates(tracks, access_token)
+        # duplicates[playlist_name] = duplicated_tracks_list
+        # utils.export_to_json_file(duplicates)
 
-with open('output_1.json', 'w') as output_file:
-    output_file.write(get_playlist.text)
 
-#2 iterate_through_playlist_and_identify_duplicated_items
-tracks = get_playlist.json().get('tracks').get('items')
-
-unique_tracks = list()
-duplicated_tracks = list()
-
-for track in tracks:
-    track_id = track.get('track').get('id')
-
-    if not track_id:
-        continue
-
-    if track_id not in unique_tracks:
-        unique_tracks.append(track_id)
-    else:
-        duplicated_tracks.append(track_id)
-
-with open('output_2.json', 'w') as output_file:
-    output_file.write(json.dumps(duplicated_tracks))
-
-#3 save_duplicated_items_to_file
-items = dict()
-
-for index, duplicate in enumerate(duplicated_tracks):
-    item = requests.get(
-        url=f"{cfg.base_url}/tracks/{duplicate}",
-        headers={ "Authorization": f"Bearer {bearer_access_token}" },
-    )
-
-    if item.status_code == 200:
-        item = item.json()
-
-        items[index] = {
-            "first_artist_name": item.get('artists')[0].get('name'),
-            "track_name": item.get('name'),
-        }
-
-with open('output_3.json', 'w') as output_file:
-
-    output_file.write(json.dumps(items, ensure_ascii=False))
