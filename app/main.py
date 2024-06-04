f78ade6dc2cf1098c308b2cfac290d743fea56d8
nlipinski <norbert.lipinski@forecast.global>
2024-06-04 12:09:23 +0100

    Uncommented main func

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     for playlist_name, playlist_id in cfg.playlists.items():
         tracks = utils.get_playlist_tracks(playlist_id, access_token)
         duplicated_tracks_list = utils.find_duplicates(tracks, access_token)
-        # duplicates[playlist_name] = duplicated_tracks_list
-        # utils.export_to_json_file(duplicates)
+        duplicates[playlist_name] = duplicated_tracks_list
+        utils.export_to_json_file(duplicates)
 
 
