2686fa3f5678b77155ac1215d178cc036868c886
nlipinski <norbert.lipinski@forecast.global>
2024-06-04 15:19:49 +0100

    Refactored

--- app/config.py
+++ app/config.py
@@ -13,9 +13,3 @@ base_url = "https://api.spotify.com/v1"
 
 client_id = os.getenv("SPOTIFY_CLIENT_ID")
 client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
-
-playlists = {
-    "main_stage": "0834B53OKVRls7gMuDwmak",
-    "old stage": "116x1msBQ3D1snxMJi7hUP",
-    "green stage": "4NECTGO4FlA8cElx9v0UlN",
-}
--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
-from app import config as cfg
 from app import utils as utils
 
+# add your playlists
+playlists = {
+    "main_stage": "0834B53OKVRls7gMuDwmak",
+    "old_stage": "116x1msBQ3D1snxMJi7hUP",
+    "green_stage": "4NECTGO4FlA8cElx9v0UlN",
+}
+
 if __name__ == "__main__":
+
     access_token = utils.get_access_token()
 
     duplicates = dict()
 
-    for playlist_name, playlist_id in cfg.playlists.items():
+    for playlist_name, playlist_id in playlists.items():
         tracks = utils.get_playlist_tracks(playlist_id, access_token)
         duplicates[playlist_name] = utils.find_duplicates(tracks, access_token)
         utils.export_to_json_file(duplicates)
 
-        # uncomment below in order to delete duplicates from the playlists
+        # uncomment in order to delete duplicates from the playlists
         # utils.delete_duplicates_from_playlist(duplicates.get(playlist_name), playlist_id, access_token)
